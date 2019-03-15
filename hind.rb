8046e85c87e9a21048a32f1f1ee825b32318b6c9
Gavin Fielder <gfielder@e1z2r7p25.42.us.org>
2019-03-15 16:12:04 -0700

    added token rescue block --2

--- hind.rb
+++ hind.rb
@@ -6,7 +6,7 @@
 #    By: gfielder <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/03/15 13:52:12 by gfielder          #+#    #+#              #
-#    Updated: 2019/03/15 16:10:28 by gfielder         ###   ########.fr        #
+#    Updated: 2019/03/15 16:11:36 by gfielder         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -56,6 +56,7 @@ secret = keys[1].chomp
 
 # Set up the client credentials
 client = OAuth2::Client.new("#{uid}", "#{secret}", site: "https://api.intra.42.fr")
+token=0
 while !token
 	begin
 		token = client.client_credentials.get_token
